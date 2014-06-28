b5fff84396bdd6fdd6c6edb6cb27594e136fd6a5
Drew Breunig <dbreunig@Drews-iMac.local>
2014-06-28 19:56:00 -0400

    Initial Commit

---
+++ BeaconNotes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeaconNotes
+//
+//  Created by Drew Breunig on 6/28/14.
+//  Copyright (c) 2014 Drew Breunig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BeaconNotes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BeaconNotes
+//
+//  Created by Drew Breunig on 6/28/14.
+//  Copyright (c) 2014 Drew Breunig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
